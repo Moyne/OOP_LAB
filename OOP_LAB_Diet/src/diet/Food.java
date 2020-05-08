ab42832ad840454394313563521f0510131e6a2a
Moyne <63983044+Moyne@users.noreply.github.com>
2020-05-08 03:21:31 +0200

    Add files via upload

---
+++ OOP_LAB_Diet/src/diet/Food.java
@@ -0,0 +1,199 @@
+package diet;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
+/**
+ * Facade class for the diet management.
+ * It allows defining and retrieving raw materials and products.
+ *
+ */
+public class Food {
+	private List<NutritionalElement> rawMats=new ArrayList<>();
+	private List<NutritionalElement> products=new ArrayList<>();
+	private List<NutritionalElement> recipes=new ArrayList<>();
+	private class Material implements NutritionalElement{
+		private String name;
+		private double calories;
+		private double proteins;
+		private double carbs;
+		private double fat;
+		private boolean per100g;
+		public Material(String name, double calories, double proteins, double carbs, double fat,boolean per100g) {
+			this.name=name;
+			this.calories=calories;
+			this.proteins=proteins;
+			this.carbs=carbs;
+			this.fat=fat;
+			this.per100g=per100g;
+		}
+		@Override
+		public String getName() {
+			return name;
+		}
+
+		@Override
+		public double getCalories() {
+			return calories;
+		}
+
+		@Override
+		public double getProteins() {
+			return proteins;
+		}
+
+		@Override
+		public double getCarbs() {
+			return carbs;
+		}
+
+		@Override
+		public double getFat() {
+			return fat;
+		}
+
+		@Override
+		public boolean per100g() {
+			return per100g;
+		}
+		@Override
+		public String toString() {
+			return name;
+		}
+	}
+	/**
+	 * Define a new raw material.
+	 * 
+	 * The nutritional values are specified for a conventional 100g amount
+	 * @param name 		unique name of the raw material
+	 * @param calories	calories per 100g
+	 * @param proteins	proteins per 100g
+	 * @param carbs		carbs per 100g
+	 * @param fat 		fats per 100g
+	 */
+	public void defineRawMaterial(String name,
+									  double calories,
+									  double proteins,
+									  double carbs,
+									  double fat){
+		rawMats.add(new Material(name,calories,proteins,carbs,fat,true));
+	}
+	
+	/**
+	 * Retrieves the collection of all defined raw materials
+	 * 
+	 * @return collection of raw materials though the {@link NutritionalElement} interface
+	 */
+	public Collection<NutritionalElement> rawMaterials(){
+		rawMats.sort((e1,e2)->e1.getName().compareTo(e2.getName()));
+		return rawMats;
+	}
+	
+	/**
+	 * Retrieves a specific raw material, given its name
+	 * 
+	 * @param name  name of the raw material
+	 * 
+	 * @return  a raw material though the {@link NutritionalElement} interface
+	 */
+	public NutritionalElement getRawMaterial(String name){
+		for(NutritionalElement a: rawMats) {
+			if(a.getName().equals(name))	return a;
+		}
+		System.err.println("Materiale non trovato");
+		return null;
+	}
+
+	/**
+	 * Define a new packaged product.
+	 * The nutritional values are specified for a unit of the product
+	 * 
+	 * @param name 		unique name of the product
+	 * @param calories	calories for a product unit
+	 * @param proteins	proteins for a product unit
+	 * @param carbs		carbs for a product unit
+	 * @param fat 		fats for a product unit
+	 */
+	public void defineProduct(String name,
+								  double calories,
+								  double proteins,
+								  double carbs,
+								  double fat){
+		products.add(new Material(name,calories,proteins,carbs,fat,false));
+	}
+	
+	/**
+	 * Retrieves the collection of all defined products
+	 * 
+	 * @return collection of products though the {@link NutritionalElement} interface
+	 */
+	public Collection<NutritionalElement> products(){
+		products.sort((e1,e2)->e1.getName().compareTo(e2.getName()));
+		return products;
+	}
+	
+	/**
+	 * Retrieves a specific product, given its name
+	 * @param name  name of the product
+	 * @return  a product though the {@link NutritionalElement} interface
+	 */
+	public NutritionalElement getProduct(String name){
+		for(NutritionalElement a: products) {
+			if(a.getName().equals(name))	return a;
+		}
+		System.err.println("Materiale non trovato");
+		return null;
+	}
+	
+	/**
+	 * Creates a new recipe stored in this Food container.
+	 *  
+	 * @param name name of the recipe
+	 * 
+	 * @return the newly created Recipe object
+	 */
+	public Recipe createRecipe(String name) {
+		Recipe recipe=new Recipe(name,this);
+		recipes.add(recipe);
+		return recipe;
+	}
+	
+	/**
+	 * Retrieves the collection of all defined recipes
+	 * 
+	 * @return collection of recipes though the {@link NutritionalElement} interface
+	 */
+	public Collection<NutritionalElement> recipes(){
+		recipes.sort((e1,e2)->e1.getName().compareTo(e2.getName()));
+		return recipes;
+	}
+	
+	/**
+	 * Retrieves a specific recipe, given its name
+	 * 
+	 * @param name  name of the recipe
+	 * 
+	 * @return  a recipe though the {@link NutritionalElement} interface
+	 */
+	public NutritionalElement getRecipe(String name){		
+		for(NutritionalElement a: recipes) {
+			if(a.getName().equals(name))	return a;
+		}
+		System.err.println("Materiale non trovato");
+		return null;
+	}
+	
+	/**
+	 * Creates a new menu
+	 * 
+	 * @param name name of the menu
+	 * 
+	 * @return the newly created menu
+	 */
+	public Menu createMenu(String name) {
+		return new Menu(name,this);
+	}
+	
+}
---
+++ OOP_LAB_Diet/src/diet/Recipe.java
@@ -0,0 +1,129 @@
+package diet;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/**
+ * Represents a recipe of the diet.
+ * 
+ * A recipe consists of a a set of ingredients that are given amounts of raw materials.
+ * The overall nutritional values of a recipe can be computed
+ * on the basis of the ingredients' values and are expressed per 100g
+ * 
+ *
+ */
+public class Recipe implements NutritionalElement {
+    private String name;
+    private Food owner;
+    private double calories;
+	private double proteins;
+	private double carbs;
+	private double fat;
+	private double quantity=0;
+    private Map<String,Double> ingredients=new HashMap<>();
+    public Recipe(String name,Food owner) {
+    	this.name=name;
+    	this.owner=owner;
+    }
+
+	/**
+	 * Adds a given quantity of an ingredient to the recipe.
+	 * The ingredient is a raw material.
+	 * 
+	 * @param material the name of the raw material to be used as ingredient
+	 * @param quantity the amount in grams of the raw material to be used
+	 * @return the same Recipe object, it allows method chaining.
+	 */
+	public Recipe addIngredient(String material, double quantity) {
+		NutritionalElement element=owner.getRawMaterial(material);
+		if(element==null) return this;
+		ingredients.put(material, quantity);
+		this.quantity+=quantity;
+		updateValues();
+		return this;
+	}
+	public void updateValues() {
+		Set<Entry<String,Double>> ing=ingredients.entrySet();
+		Iterator<Entry<String,Double>> it=ing.iterator();
+		double newCalories=0;
+		double newProteins=0;
+		double newCarbs=0;
+		double newFat=0;
+		while(it.hasNext()) {
+			Entry<String,Double> el=it.next();
+			NutritionalElement element=owner.getRawMaterial(el.getKey());
+			newCalories+=element.getCalories()*el.getValue()/quantity;
+			newProteins+=element.getProteins()*el.getValue()/quantity;
+			newCarbs+=element.getCarbs()*el.getValue()/quantity;
+			newFat+=element.getFat()*el.getValue()/quantity;
+		}
+		calories=newCalories;	proteins=newProteins;	carbs=newCarbs;	fat=newFat;
+		return;
+	}
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public double getCalories() {
+		return calories;
+	}
+
+	@Override
+	public double getProteins() {
+		return proteins;
+	}
+
+	@Override
+	public double getCarbs() {
+		return carbs;
+	}
+
+	@Override
+	public double getFat() {
+		return fat;
+	}
+
+	/**
+	 * Indicates whether the nutritional values returned by the other methods
+	 * refer to a conventional 100g quantity of nutritional element,
+	 * or to a unit of element.
+	 * 
+	 * For the {@link Recipe} class it must always return {@code true}:
+	 * a recipe expresses nutritional values per 100g
+	 * 
+	 * @return boolean indicator
+	 */
+	@Override
+	public boolean per100g() {
+		return true;
+	}
+	
+	
+	/**
+	 * Returns the ingredients composing the recipe.
+	 * 
+	 * A string that contains all the ingredients, one per per line, 
+	 * using the following format:
+	 * {@code "Material : ###.#"} where <i>Material</i> is the name of the 
+	 * raw material and <i>###.#</i> is the relative quantity. 
+	 * 
+	 * Lines are all terminated with character {@code '\n'} and the ingredients 
+	 * must appear in the same order they have been added to the recipe.
+	 */
+	@Override
+	public String toString() {
+		Set<Entry<String,Double>> ing=ingredients.entrySet();
+		Iterator<Entry<String,Double>> it=ing.iterator();
+		StringBuffer x=new StringBuffer();
+		while(it.hasNext()) {
+			Entry<String,Double> el=it.next();
+			x.append(el.getKey()+" : "+el.getValue()+"\n");
+		}
+		return x.toString();
+	}
+}
